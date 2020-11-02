058e585671cac6a0811531a65e7da2b2006a5ee6
sergej <tulitas1@gmail.com>
2020-11-02 23:10:21 +0200

    New application spring with angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+  title: string;
+
+  constructor() {
+    this.title = 'Spring Boot - Angular Application';
+  }
+
 }
